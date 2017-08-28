fb9684d43285cfb74b03588761377fffd26195b4
Jakub Krawczyk <jakubkrawczyk2014@gmail.com>
2017-08-28 16:15:22 +0200

    Tworzenie logowania

--- app/src/main/java/net/pclogger/pclogger/LogicActivity.java
+++ app/src/main/java/net/pclogger/pclogger/LogicActivity.java
@@ -2,12 +2,17 @@ package net.pclogger.pclogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+
+import static android.support.v7.appcompat.R.styleable.View;
 
 public class LogicActivity extends AppCompatActivity {
 
@@ -26,5 +31,24 @@ public class LogicActivity extends AppCompatActivity {
     private void setControls(){
         EditText password = (EditText) findViewById(R.id.editTextPassword);
         password.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    private boolean validateEmail(){
+        String email = _editTextLogin.getText().toString();
+        _editTextLogin.setError(null);
+
+        if(TextUtils.isEmpty(email)){
+            _editTextLogin.setError("Podaj email");
+            return false;
+        }else{
+            return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+    }
+
+    @OnClick(R.id.buttonLogin)
+    public void submit(Button button){
+        if(validateEmail()){
+            //TODO ODPYTAJ API
+    }
     }
 }
